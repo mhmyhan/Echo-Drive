05b63f8fdc003b96e875c3c6005c7fffe3040eb9
mhmyhan <max.myhan@gmail.com>
2024-03-26 23:28:22 +0000

    first commit - Max

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,10 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println(STR."i = \{i}");
+
+            String myString = "Waluigi";
+
+            System.out.println(myString);
         }
     }
 }
