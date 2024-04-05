535c100ea25687cd1d721f10183b68269eb4c5f1
Ali <adadashipour@gmail.com>
2024-04-05 21:15:17 +0100

    put back EchoDrive

---
+++ core/src/com/echodrive/game/EchoDrive.java
@@ -0,0 +1,17 @@
+package com.echodrive.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.*;
+
+public class EchoDrive extends ApplicationAdapter {
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+}
